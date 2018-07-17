128055df6ffbe06ef1a717d8310c5ecac20c20dc
noaaroland <roland.schweitzer@noaa.gov>
2018-07-17 10:47:39 -0500

    Data structures to send analysis requests.

---
+++ grails-app/domain/pmel/sdig/las/Analysis.groovy
@@ -0,0 +1,16 @@
+package pmel.sdig.las
+
+class Analysis {
+    String transformation
+    String axes
+    List analysisAxes
+    static hasMany = [analysisAxes: AnalysisAxis]
+    String hash() {
+        String hash = transformation+"_"+axes+"_"
+        for (int i = 0; i < analysisAxes.size(); i++) {
+            AnalysisAxis a = analysisAxes.get(i)
+            hash = hash + a.getType()+"_"+a.getLo()+"_"+a.getHi()
+        }
+        JDOMUtils.MD5Encode(hash)
+    }
+}

9de894f81c84008f577574932b7b97682ebd6821
noaaroland <roland.schweitzer@noaa.gov>
2019-03-22 15:37:43 -0500

    Return a data set rendere with the JSON views system to avoid problems with the deep

--- grails-app/controllers/pmel/sdig/las/SearchController.groovy
+++ grails-app/controllers/pmel/sdig/las/SearchController.groovy
@@ -11,23 +11,15 @@ class SearchController {
 
         def results = Dataset.search(searchQuery)
 
-        Dataset resultsDataset = new Dataset([title: "Search Results"])
+        Dataset dataset = new Dataset([title: "Search Results"])
         if ( results.total == 0 ) {
-            resultsDataset.setMessage("No results found matching search.")
+            dataset.setMessage("No results found matching search.")
         }
-        boolean deep = true;
         results.getSearchResults().each {Dataset result ->
-            deep = deep && result.variableChildren
-            resultsDataset.addToDatasets(result)
+            dataset.addToDatasets(result)
         }
-        resultsDataset.setVariableChildren(deep)
-        if ( resultsDataset.variableChildren ) {
-            JSON.use("deep") {
-                render resultsDataset as JSON
-            }
-        } else {
-            respond resultsDataset, formats: ['json'], view: 'dataset'
-        }
+
+        respond dataset, formats: ['json'], view: 'dataset'
 
     }
 }

de3721b4ecafecf1daf324cd0ed1927729e8496e
noaaroland <roland.schweitzer@noaa.gov>
2019-03-22 18:22:30 -0500

    Make a new result set for the thumbnail product.

--- grails-app/services/pmel/sdig/las/ResultsService.groovy
+++ grails-app/services/pmel/sdig/las/ResultsService.groovy
@@ -23,6 +23,18 @@ class ResultsService {
         results_debug_image_mapscale_annotations
 
     }
+    def getThumbnailResults() {
+        ResultSet xy_thumb = new ResultSet([name: "results_xy_thumbnail"])
+        Result plot_image = new Result([name: "plot_image", mime_type: "image/png", type: "image", file_type: "png", suffix: ".png"])
+        Result debug = new Result([name: "debug", mime_type: "text/plain", type: "debug", file_type: "text", suffix: ".txt"])
+        Result map_scale = new Result([name: "map_scale", mime_type: "text/xml", type: "map_scale", file_type: "txt", suffix: ".xml"])
+        Result annotations = new Result([name: "annotations", mime_type: "text/xml", type: "annotations", file_type: "xml", suffix: ".xml"])
+        xy_thumb.addToResults(plot_image)
+        xy_thumb.addToResults(debug)
+        xy_thumb.addToResults(map_scale)
+        xy_thumb.addToResults(annotations)
+        xy_thumb
+    }
     def getAnimateSetupResults() {
 
         ResultSet results = new ResultSet([name: "results_debug_frames_list"])
